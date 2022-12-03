766907193cd15cab561b77d46695f6f03d2a66d2
Mark F <exactmark@gmail.com>
2022-12-03 11:28:37 -0500

    Day 2 start

---
+++ template.py
@@ -0,0 +1,15 @@
+from inputReader import read_file
+
+
+def solve_pt_1(input_path):
+    input_lines = read_file(input_path)
+
+
+def solve_pt_2(input_path):
+    input_lines = read_file(input_path)
+
+
+day = "02"
+sample_path = "inputDir/" + day + "_Sample.txt"
+data_path = "inputDir/" + day + "_Data.txt"
+solve_pt_1(data_path)
